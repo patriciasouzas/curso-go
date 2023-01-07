be75e4c92d99981c99a52642ecaa03927d65397f
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-06 23:52:40 -0300

    variáveis e atribuição

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+var (
+	nome string
+	n1   int
+	n2   int32
+)
+
 func main() {
-	fmt.Println("Hello word!")
+	mensagem := "Aula 03 - Go 101" //declaração com atribuição
+	fmt.Println(mensagem)
+
+	var b, f, s = true, 2.3, "Olá"
+	fmt.Println(b, f, s)
+
+	var total int
+	total++ //atribuição no valor default
+	fmt.Println("total:", total)
+
+	nome = "Patricia"
+	fmt.Println("Hello,", nome, "!")
+
+	var x, y = 10, 20
+	fmt.Println(x, y)
+	x, y = y, x //brincando com substituição de valores
+	fmt.Println(x, y)
 }
