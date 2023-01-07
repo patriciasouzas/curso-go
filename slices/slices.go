ec862a9319fc771359613e6f1f58221a9cbc093f
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-07 17:48:13 -0300

    treinando maps

--- slices/slices.go
+++ slices/slices.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//slices
 	nomes := []string{"Paulo", "João", "Dani", "Pedro", "Carla"}
 	fmt.Println(nomes, len(nomes), cap(nomes))
 
@@ -13,7 +14,23 @@ func main() {
 	fmt.Println(nomes, len(nomes), cap(nomes))
 
 	//definindo tipo, tamanho e capacidade do slice
-	idades := make([]int, 4, 10)
-	fmt.Println(idades)
+	nums := make([]int, 4, 10)
+	fmt.Println(nums)
 
+	//maps
+	idades := make(map[string]uint8)
+	idades["Tiago"] = 15
+	idades["Carlos"] = 35
+	idades["Maria"] = 32
+	idades["Amanda"] = 34
+
+	fmt.Println(idades)
+	fmt.Println(idades["Maria"])
+	fmt.Println(idades["Xavier"]) //quando não tem o valor no map, ele retorna o default da chave
+
+	val, ok := idades["Tiago"] //valor existe
+	fmt.Println(val, ok)
+
+	val, ok1 := idades["Lucas"] //valor não existe
+	fmt.Println(val, ok1)
 }
