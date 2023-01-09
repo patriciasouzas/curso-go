f8d0fcacebc8f6811aab6f92bf6a0ebd24153338
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-08 23:57:19 -0300

    otimizando channels com buffer

---
+++ channelComBuffer/channelBuffer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros(n chan<- int) {
+	for i := 0; i < 10; i++ {
+		n <- i
+		fmt.Printf("Escrito no channel: %d\n", i)
+		time.Sleep(time.Millisecond * 90)
+	}
+	fmt.Println("fim da escrita")
+	close(n)
+}
+
+func main() {
+	cn := make(chan int, 3)
+	go numeros(cn)
+
+	for v := range cn {
+		fmt.Printf("lido do channel: %d\n", v)
+		time.Sleep(time.Millisecond * 180)
+	}
+
+	fmt.Println("fim da execução")
+}
