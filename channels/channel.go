3fd3781c1f0d1065b27372a71012568163d94fb0
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-08 23:43:04 -0300

    conhecendo channel

---
+++ channels/channel.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros(done chan<- bool) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", i)
+		time.Sleep(time.Millisecond * 150)
+	}
+	done <- true
+}
+
+func letras(done chan<- bool) {
+	for l := 'a'; l < 'j'; l++ {
+		fmt.Printf("%c ", l)
+		time.Sleep(time.Millisecond * 200)
+	}
+	done <- true
+}
+
+func main() {
+	cn := make(chan bool)
+	cl := make(chan bool)
+
+	go numeros(cn)
+	go letras(cl)
+
+	<-cn
+	<-cl
+
+	fmt.Println("fim da execução")
+}
